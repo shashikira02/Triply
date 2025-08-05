c43958b070b0794acb26e2c48f506870c8b49e75
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-08-05 18:19:32 +0530

    Update index.js

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,5 +1,5 @@
 
 // const config = { backendEndpoint: "http://127.0.0.1:8082/" };
-const config = { backendEndpoint: "https://triply-6vue.onrender.com" };
+const config = { backendEndpoint: "https://triply-6vue.onrender.com/" };
 
 export default config;
